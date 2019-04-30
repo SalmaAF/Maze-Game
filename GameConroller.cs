b4caf09263160491db7f0c50ee7445569e71d80d
Salma Alfohaid <48376538+SalmaAF@users.noreply.github.com>
2019-04-30 15:26:02 +0300

    Scrips

---
+++ GameConroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameConroller : MonoBehaviour {
+
+	public Text timer;
+	float timeLeft = 50.0f;
+
+	public GameObject Panel;
+	public GameObject gameOver;
+	public GameObject win;
+
+	private AudioSource audio;
+
+	void Start () {
+		Time.timeScale = 1f;
+		audio = GetComponent<AudioSource> ();
+		audio.mute = false;
+	}
+
+	void Awake () {
+		Panel.SetActive (false);//don't show win panel
+		win.SetActive (false);//don't show win message
+		gameOver.SetActive (false);//don't show gameover panel
+
+	}
+
+	void Update () {
+		SetTimerText ();//timer
+	}
+
+	void SetTimerText ()
+	{//showing timer 
+		timeLeft -= Time.deltaTime;
+		timer.text = "Timer: " + Mathf.Round(timeLeft);
+		if(timeLeft < 0)
+		{
+			Time.timeScale = 0f;
+			Panel.SetActive (true);//show win panel
+			gameOver.SetActive (true);//show gameover panel	
+			audio.mute = true;
+
+		}}
+
+
+	void  OnTriggerEnter2D(Collider2D other) 
+		{
+			if(other.tag == "Player"){
+			Time.timeScale = 0f;
+				Panel.SetActive (true);//show win panel
+				win.SetActive (true);//show win panel
+			audio.mute = true;
+
+			}}
+
+		public void PlayAgain () {
+		Application.LoadLevel (Application.loadedLevel);
+		}
+}
---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	private Rigidbody2D rb;
+	private Animator ani;
+
+	public float speed;
+
+	private bool facingRight;
+
+	void Start () {
+		facingRight = true;
+		rb = GetComponent<Rigidbody2D> ();//player
+		ani = GetComponent<Animator> ();
+
+	}
+	
+
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");	
+
+		Movement (horizontal, vertical);
+		Flip(horizontal);
+	}
+
+	void Movement(float horizontal, float vertical ){
+		rb.velocity = new Vector2 (horizontal*speed,vertical*speed);//move the player by arrows
+		ani.SetFloat("speed2",Mathf.Abs( vertical));
+		ani.SetFloat("speed",Mathf.Abs( horizontal));//to change the movement to run
+	}
+
+	void Flip(float horizontal ){//to make the player flip
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+}
